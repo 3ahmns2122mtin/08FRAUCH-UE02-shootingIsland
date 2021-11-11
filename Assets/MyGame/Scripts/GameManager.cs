78e5e3a353eda9eade9aa8c41232782e4f76c780
FRAUCH1 <90834237+FRAUCH1@users.noreply.github.com>
2021-11-11 10:33:19 +0100

    Add files via upload

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject target;
     public GameObject parentOfTargets;
+    public GameObject objCounter;
 
+    private Text textCounter;
     public bool won;
     public int score;
     // Start is called before the first frame update
     void Start()
     {
+        textCounter = objCounter.GetComponent<Text>();
         won = false;
         InvokeRepeating("Spawn", 1f, 2f);
     }
@@ -52,6 +56,7 @@ public class GameManager : MonoBehaviour
     {
         score++;
         Debug.Log("increment ..." + score);
+        textCounter.text = score.ToString();
 
         if(score == 10)
         {
